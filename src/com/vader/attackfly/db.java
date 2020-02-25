4093ee8b46e921bf2c6da37019a2f0a37314d9cc
CircleMonogatari <circlemono@outlook.com>
2020-02-26 03:57:48 +0800

    添加数据库

---
+++ src/com/vader/attackfly/db.java
@@ -0,0 +1,11 @@
+package com.vader.attackfly;
+
+import java.sql.*;
+
+public class db {
+	void init() {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		 conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test_demo?useSSL=false&serverTimezone=UTC","root","password");	
+	}
+	
+}
